cc144fbea77b89f69b983979de74e5be67090788
mona2989lisa <mona2989lisa@gmail.com>
2016-12-12 00:51:53 -0600

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,21 @@
+package main.java.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Spring Application: Target Assignment with Gradle and Docker";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
